8b1ba75441350c26e7177fc11d50f594a6f6691b
ohba <ohba@sendai-nct.ac.jp>
2025-01-22 08:38:46 +0900

    取り出し方法の追加

--- tuple.rs
+++ tuple.rs
@@ -2,4 +2,10 @@ fn main()
 {
 	let tuple = (5, 3.14, '😻');
 	println!("{:?}",tuple);
+
+	let assign: (i32, f64, char, bool) = (2, 2.71, '😇',true);
+	let (w, x, y, z) = assign;
+	println!("w:{}, x:{}, y:{}, z:{}",w,x,y,z);
+
+	println!("3番目は{}",tuple.2);
 }
